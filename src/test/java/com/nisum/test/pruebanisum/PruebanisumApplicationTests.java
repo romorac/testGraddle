68124c74f96246219672f835bd67dbc11397d7e6
rodolfo <rmorac@gmail.com>
2020-08-05 22:21:47 -0400

    fin desarrollo

---
+++ src/test/java/com/nisum/test/pruebanisum/PruebanisumApplicationTests.java
@@ -0,0 +1,33 @@
+package com.nisum.test.pruebanisum;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@SpringBootTest
+class PruebanisumApplicationTests {
+	@Autowired
+	private UsuarioService userServ;
+
+	void contextLoads() {
+	}
+
+	@Test
+	public void addUsuario() {
+		assertTrue(true);
+//		UsuarioDTO nuevo = new UsuarioDTO();
+//		nuevo.setName("rodolfo Mora");
+//		nuevo.setPassword("Saaaaa78");
+//		nuevo.setEmail("rmorac@gmail.com");
+//		UsuarioBO test = userServ.addUsuario(nuevo);
+//		assertTrue(userServ.addUsuario(nuevo) != null);
+
+//		if (null != test) {
+//			assertTrue(true);
+//		} else {
+//			assertFalse(false);
+//		}
+	}
+
+}
